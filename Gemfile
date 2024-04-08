cb6e2f805b5adde8e1bc86ba6ead1a415a008c7b
Ruxadev <grmdev01@gmail.com>
2024-04-08 20:50:09 -0300

    Building a Show Article

--- Gemfile
+++ Gemfile
@@ -25,5 +25,6 @@ end
 group :development do
   # Speed up commands on slow machines / big apps [https://github.com/rails/spring]
   # gem "spring"
+  gem 'byebug', '~> 11.1'
 end
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_04_08_191137) do
+ActiveRecord::Schema[7.1].define(version: 2024_04_08_191956) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
-    t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.datetime "created_at", null: false
   end
 
 end
